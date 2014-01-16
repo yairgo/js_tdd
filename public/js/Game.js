ebb57b3bd89d956e8516ffa84fca079e21f67304
Jason Mayer <jason.mayer@gmail.com>
2014-01-16 17:23:31 -0600

    failing tests for the game object

---
+++ public/js/Game.js
@@ -0,0 +1,18 @@
+function Game() {
+    this.frames = [];
+}
+Game.prototype.getTotalScore = function() {
+    return 0;
+};
+
+Game.prototype.getFrames = function() {
+    return 0;
+};
+
+Game.prototype.getScoreForFrame = function() {
+    return 0;
+};
+
+Game.prototype.addFrame = function() {
+    return 0;
+};
---
+++ spec/javascripts/GameSpec.js
@@ -0,0 +1,39 @@
+describe("Game", function() {
+  var myGame;
+
+  beforeEach(function() {
+    myGame = new Game();
+  });
+
+  describe("can add a frame", function() {
+    var frame;
+    beforeEach(function() {
+      frame = new Frame();
+      frame.addScore(10);
+      myGame.addFrame(frame);
+    });
+
+    it("adds the frame", function() {
+      var actual = myGame.getFrames()[0];
+      expect(actual).toEqual(frame);
+    });
+  });
+  
+  describe("can get the total score", function() {
+    beforeEach(function() {
+      
+    });
+
+    it("should score 10 points", function() {
+    });
+  });
+  
+  describe("can get the score for a frame", function() {
+    beforeEach(function() {
+      
+    });
+
+    it("should score 10 points", function() {
+    });
+  });
+});
