05a28872dd38860b0a8583e81508dab3bab12734
Jason Mayer <jason.mayer@gmail.com>
2014-01-16 17:09:42 -0600

    adding non strike, non spare scenario to the tests

--- spec/javascripts/FrameSpec.js
+++ spec/javascripts/FrameSpec.js
@@ -27,7 +27,7 @@ describe("Frame", function() {
     });
   });
   
-  describe("when the first two frame equal 10 pins", function() {
+  describe("when the first two frames equal 10 pins", function() {
     beforeEach(function() {
       myFrame.addScore(7);
       myFrame.addScore(3);
@@ -47,6 +47,29 @@ describe("Frame", function() {
     
     it("should score 10 points", function() {
       expect(myFrame.getScore()).toEqual(10);
+    });
+  });
+  
+  describe("when the first two frames equal 9 pins", function() {
+    beforeEach(function() {
+      myFrame.addScore(6);
+      myFrame.addScore(3);
+    });
+
+    it("should not be a strike", function() {
+      expect(myFrame.isStrike()).toBeFalsy();
+    });
+
+    it("should be the end of the turn", function() {
+      expect(myFrame.isTurnOver()).toBeTruthy();
+    });
+    
+    it("should be a spare", function() {
+      expect(myFrame.isSpare()).toBeFalsy();
+    });
+    
+    it("should score 9 points", function() {
+      expect(myFrame.getScore()).toEqual(9);
     });
   });
 });
