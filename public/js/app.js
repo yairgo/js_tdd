cc7c98e454ef8756c3c95a3f6e36719dff091ae0
Kenny Ortmann <kenny.ortmann@gmail.com>
2014-01-16 18:26:36 -0600

    adding todo

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
+var game;
 $(function() {
 
-  var game = new Game();
+  game = new Game();
   var inputParser = new InputParser(game);
   $('.frame input').focusout(function(){
     var kids = $(this).parent().children();
